306127187c697ca95bb7a070a81f635d58386f38
rgalan <rafalgalan94@gmail.com>
2024-11-15 18:29:26 +0000

    modify test files

--- test/selenium/pythonWebTest.py
+++ test/selenium/pythonWebTest.py
@@ -3,19 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from os import environ
 from selenium.webdriver.common.by import By
-
+ 
 application_URL = environ.get('APPLICATION_URL', 'http://192.168.44.44:5000/')
 selenium_URL = environ.get('SELENIUM_URL', 'http://192.168.44.44:4444/wd/hub')
-
-
+ 
+ 
 class PythonOrgSearch(unittest.TestCase):
-
+ 
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        # self.driver = webdriver.Remote(
-        #     command_executor=selenium_URL,
-        #     options=webdriver.FirefoxOptions())
-
+        # self.driver = webdriver.Firefox()
+        self.driver = webdriver.Remote(
+            command_executor=selenium_URL,
+            options=webdriver.FirefoxOptions())
+ 
     def test_search_in_python_org(self):
         driver = self.driver
         driver.implicitly_wait(5)
@@ -26,7 +26,7 @@ class PythonOrgSearch(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         self.assertNotIn("No results found.", driver.page_source)
         driver.quit()
-
-
+ 
+ 
 if __name__ == "__main__":
     unittest.main()
